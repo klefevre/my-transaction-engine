988d537a919510a5665e7eafc790c18ff218e2a8
Kevin Lefèvre <1300874+klefevre@users.noreply.github.com>
2022-06-10 03:31:44 +0200

    Use of the engine

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::fs;
+use std::io;
 use std::path;
+use std::process;
 
 use anyhow::Result;
 use clap::{Parser, ValueHint};
 use log::debug;
+
+use my_transaction_engine::prelude::*;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -21,5 +26,15 @@ fn main() -> Result<()> {
     let args = Args::parse();
     debug!("args: {:?}", args);
 
+    let input = fs::File::open(args.path)?;
+    let output = io::stdout();
+
+    if let Err(err) = try_run(input, output) {
+        eprintln!("Error: {}", err);
+        err.chain()
+            .skip(1)
+            .for_each(|cause| eprintln!("because: {}", cause));
+        process::exit(1);
+    }
     Ok(())
 }
