8fb599f42496be271d7ef29c1dc90d0f7fd4e604
Kevin Lefèvre <1300874+klefevre@users.noreply.github.com>
2022-06-10 03:29:30 +0200

    Add arguments parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::path;
+
+use anyhow::Result;
+use clap::{Parser, ValueHint};
+use log::debug;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// CSV path
+    #[clap(parse(from_os_str), value_hint = ValueHint::FilePath)]
+    path: path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    env_logger::builder()
+        .format_module_path(false)
+        .format_timestamp(None)
+        .init();
+
+    let args = Args::parse();
+    debug!("args: {:?}", args);
+
+    Ok(())
 }
